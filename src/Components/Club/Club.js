3a28f80ba33b22e1e8d10445414c73cb9959433b
Raifurrahim <raifurrahimcse13@gmail.com>
2021-07-28 08:19:31 +0600

    added map

--- src/Components/Club/Club.js
+++ src/Components/Club/Club.js
@@ -5,7 +5,7 @@ import { faTrophy,faUsers,faEuroSign,faIdCard } from '@fortawesome/free-solid-sv
 
 const Club = (props) => {
     const club = props.club;
-    const totalBudget = club.reduce((sum, player) =>sum+player.salary, 0)
+    const totalBudget = club.reduce((totalBudget, player) =>totalBudget+player.salary, 0)
     // let totalBudget = 0;
     // for(let i = 0; i < club.length; i++) {
     //     const player = club[i];
@@ -15,10 +15,16 @@ const Club = (props) => {
     return (
         <div className="club-component">
            <h1><FontAwesomeIcon icon={faTrophy}></FontAwesomeIcon> Club details</h1>
-           <p> Total selected players: <FontAwesomeIcon icon={faUsers}></FontAwesomeIcon> {club.length}</p>
-           <p><FontAwesomeIcon icon={faIdCard}></FontAwesomeIcon> Name: {props.name}</p>
-           <p>Salary: {club.salary}</p>
-           <p>Total Budget: <FontAwesomeIcon icon={faEuroSign}></FontAwesomeIcon> {totalBudget}</p>
+           <h4> Total selected players: <FontAwesomeIcon icon={faUsers}></FontAwesomeIcon> {club.length}</h4>
+           <h4><FontAwesomeIcon icon={faIdCard}></FontAwesomeIcon> Name: </h4><br />
+           {
+               club.map((club)=>(<li>{club.name} <br /><h5>salary:</h5> {club.salary}</li>))
+           }
+           {/* <p>Salary:</p>
+           {
+               club.map((club)=>(<li><FontAwesomeIcon icon={faEuroSign}></FontAwesomeIcon> {club.salary}</li>))
+           } */}
+           <h4>Total Budget: <FontAwesomeIcon icon={faEuroSign}></FontAwesomeIcon> {totalBudget}</h4>
         </div>
     );
 };
